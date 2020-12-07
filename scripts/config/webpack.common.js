e79af3e2a7d52666a0121c120f2e69d5b866f10a
jiaan.zja <jiaan.zja@alibaba-inc.com>
2020-12-08 00:10:38 +0800

    feat: css-loader

--- scripts/config/webpack.common.js
+++ scripts/config/webpack.common.js
@@ -1,12 +1,54 @@
 const path = require('path')
-const { PROJECT_PATH } = require('../constants')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { PROJECT_PATH, isDev } = require('../constants')
 
 module.exports = {
 	entry: {
 		app: path.resolve(PROJECT_PATH, './src/app.js'),
 	},
 	output: {
-		filename: 'js/[name].[hash:8].js',
+		filename: `js/[name]${isDev ? '' : '.[hash:8]'}.js`,
 		path: path.resolve(PROJECT_PATH, './dist'),
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.resolve(PROJECT_PATH, './public/index.html'),
+			filename: 'index.html',
+			cache: false,
+			minify: isDev
+				? false
+				: {
+						removeAttributeQuotes: true,
+						collapseWhitespace: true,
+						removeComments: true,
+						collapseBooleanAttributes: true,
+						collapseInlineTagWhitespace: true,
+						removeRedundantAttributes: true,
+						removeScriptTypeAttributes: true,
+						removeStyleLinkTypeAttributes: true,
+						minifyCSS: true,
+						minifyJS: true,
+						minifyURLs: true,
+						useShortDoctype: true,
+				  },
+		}),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [
+					'style-loader',
+					{
+						loader: 'css-loader',
+						options: {
+							modules: false, // 默认就是 false, 若要开启，可在官网具体查看可配置项
+							sourceMap: isDev, // 开启后与 devtool 设置一致, 开发环境开启，生产环境关闭
+							importLoaders: 0, // 指定在 CSS loader 处理前使用的 laoder 数量
+						},
+					},
+				],
+			},
+		],
 	},
 }
--- scripts/config/webpack.dev.js
+++ scripts/config/webpack.dev.js
@@ -1,6 +1,17 @@
 const { merge } = require('webpack-merge')
 const common = require('./webpack.common')
+const { SERVER_HOST, SERVER_PORT } = require('../constants')
 
 module.exports = merge(common, {
 	mode: 'development',
+	devtool: 'eval-source-map',
+	devServer: {
+		host: SERVER_HOST,
+		port: SERVER_PORT,
+		stats: 'errors-only',
+		clientLogLevel: 'silent',
+		compress: true,
+		open: true,
+		hot: true,
+	},
 })
